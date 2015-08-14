5571898891e886a4826641f4009c5b34b74e6cf0
Joe Lei <thezero12@hotmail.com>
2015-08-15 01:48:56 +0800

    roman assert

--- Lib/roman.py
+++ Lib/roman.py
@@ -2,7 +2,10 @@
 # Copyright 2015 IFOOTH
 # Author: Joe Lei <thezero12@hotmail.com>
 """罗马数字转换"""
-_ROMAN = (
+import re
+
+
+_Roman = (
     ('M', 1000),
     ('CM', 900),
     ('D', 500),
@@ -17,12 +20,25 @@ _ROMAN = (
     ('IV', 4),
     ('I', 1)
 )
+_RomanPattern = re.compile("""
+    ^                   # beginning of string
+    M{0,4}              # thousands - 0 to 4 M's
+    (CM|CD|D?C{0,3})    # hundreds - 900 (CM), 400 (CD), 0-300 (0 to 3 C's),
+                        #            or 500-800 (D, followed by 0 to 3 C's)
+    (XC|XL|L?X{0,3})    # tens - 90 (XC), 40 (XL), 0-30 (0 to 3 X's),
+                        #        or 50-80 (L, followed by 0 to 3 X's)
+    (IX|IV|V?I{0,3})    # ones - 9 (IX), 4 (IV), 0-3 (0 to 3 I's),
+                        #        or 5-8 (V, followed by 0 to 3 I's)
+    $                   # end of string
+    """, re.VERBOSE)
 
 
 def int2roman(num):
-    # todo 类型，边界检查
+    assert 0 < num < 5000, 'num must be 1 - 4999'
+    assert int(num) == num, 'num must be int'
+
     result = ''
-    for n, i in _ROMAN:
+    for n, i in _Roman:
         while num >= i:
             result += n
             num -= i
@@ -30,9 +46,12 @@ def int2roman(num):
 
 
 def roman2int(roman):
+    assert roman, 'roman cannt be null'
+    assert _RomanPattern.match(roman), 'invalid roman'
+
     result = 0
     index = 0
-    for n, i in _ROMAN:
+    for n, i in _Roman:
         while roman[index: index + len(n)] == n:
             result += i
             index += len(n)
