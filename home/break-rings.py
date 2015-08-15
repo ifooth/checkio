7154aa25d7e1193824450a61b9c322e82f53f346
Joe Lei <thezero12@hotmail.com>
2015-08-15 13:34:54 +0800

    finish break_rings

--- home/break-rings.py
+++ home/break-rings.py
@@ -1,9 +1,36 @@
 # -*- coding: utf-8 -*-
 # Copyright 2015 IFOOTH
 # Author: Joe Lei <thezero12@hotmail.com>
+import copy
 
 
 def break_rings(rings):
+    vertex = {}
+    rings = list(rings)
+    for i in rings:
+        for j in i:
+            vertex[j] = vertex.get(j, 0) + 1
+    _break = 0
+    while True:
+        if sum(vertex.values()) == 0:
+            break
+        _min = min(filter(lambda x: vertex[x] >= 1, vertex), key=lambda x: vertex[x])
+        _need_delete = set()
+        for i in rings:
+            if _min in i:
+                _need_delete |= i
+        _need_delete -= {_min}
+        for other in _need_delete:
+            for j in copy.deepcopy(rings):
+                if _min not in j and other in j:
+                    o = j - {other}
+                    o = o.pop()
+                    vertex[o] -= 1
+                    rings.remove(j)
+            vertex[_min] -= 1
+            vertex.pop(other)
+            _break += 1
+    return _break
 
 
 if __name__ == '__main__':
