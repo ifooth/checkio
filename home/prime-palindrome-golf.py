fae3c9b622dec94b0fd4f11fdc04ca17735931bd
Joe Lei <thezero12@hotmail.com>
2015-08-17 23:38:22 +0800

    improve prime-palindrome-golf

--- home/prime-palindrome-golf.py
+++ home/prime-palindrome-golf.py
@@ -1,2 +1,1 @@
-def golf(number):
-    return iter(i for i in range(number + 1, 986899) if str(i) == ''.join(reversed(str(i))) and all((i % j != 0 for j in range(2, i)))).next()
+def golf(n):return iter(i for i in range(n+1,986899) if str(i)==str(i)[::-1] and all(i%j!=0 for j in range(2,i))).next()
