622420600ee6603e75ae0896622848661155552c
Joe Lei <thezero12@hotmail.com>
2015-08-20 01:44:55 +0800

    add setup script

---
+++ checkio/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 IFOOTH
+# Author: Joe Lei <thezero12@hotmail.com>
+
+__VERSION__ = '1.0'
---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import os.path
+from setuptools import setup, find_packages
+
+
+version = __import__('checkio').__VERSION__
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as fh:
+    long_description = fh.read()
+
+setup(
+    name="checkio",
+    version=version,
+    packages=find_packages(),
+    author="Joe Lei",
+    author_email="thezero12@hotmail.com",
+    description="checkio solution",
+    long_description=long_description,
+    license="IFOOTH",
+    keywords="python checkio",
+    url="http://github.com/ifooth/checkio",
+    classifiers=[
+        'Programming Language :: Python',
+    ],
+)
